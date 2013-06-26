8f96d277ca14beeda9960d26131aadcb3bff4f24
bohyun <rlaqhgus0627@naver.com>
2013-06-26 20:37:11 +0900

    프로젝트 생성

--- src/com/example/worryeraser/worry_input.java
+++ src/com/example/worryeraser/worry_input.java
@@ -1,6 +1,7 @@
 package com.example.worryeraser;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -12,12 +13,14 @@ public class worry_input extends Activity{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		Intent intent = getIntent();
+		
 		ImageButton btnArrow = (ImageButton)findViewById(R.id.arrow);
 		btnArrow.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				
+	
 			}
 		});
 	}
